79c89cc2190f995874004aa67fe5d103cf02f891
Salome Braganza <salomekbg@gmail.com>
2016-07-26 20:00:27 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,41 @@
 def consolidate_cart(cart)
-  # code here
+  final = {}
+  cart.each {|item_info| item_info.each {|item, info|
+      if final.include?(item) == false
+        final[item] = info
+        final[item][:count] = 1
+      else
+        final[item][:count] += 1
+      end
+    }}
+  final
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  final = cart.clone
+  cart.each {|item, data| coupons.each {|coupon|
+      if coupon[:item] == item && coupon[:num] <= final[item][:count] && final.keys.include?("#{item} W/COUPON") == false
+        final[item][:count] -= coupon[:num]
+        final["#{item} W/COUPON"] = {:price => coupon[:cost], :clearance => data[:clearance], :count => 1}
+      elsif coupon[:item] == item && coupon[:num] <= final[item][:count] && final.keys.include?("#{item} W/COUPON")
+        final[item][:count] -= coupon[:num]
+        final["#{item} W/COUPON"][:count] = final["#{item} W/COUPON"][:count] + 1
+      end
+    }}
+   final
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each {|item, data| data[:price] -= (data[:price] * 0.20) if data[:clearance] == true}
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  new_cart = consolidate_cart(cart)
+  new_cart = apply_coupons(new_cart, coupons)
+  new_cart = apply_clearance(new_cart)
+  total = 0
+  new_cart.each {|item, data| total += (data[:price] * data[:count])}
+  total -= total * 0.10 if total > 100
+  total
 end
